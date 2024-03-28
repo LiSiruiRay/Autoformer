a8946bc60081c52ae57e55d4dd6d6d1e0a54ef47
Sirui Li <slray@nyu.edu>
2024-03-29 00:15:15 +0400

    exclude big data file

---
+++ test_auto_exp_script.py
@@ -0,0 +1,20 @@
+# Author: ray
+# Date: 3/29/24
+
+import unittest
+
+from automate_experiments_fed import store_meta_info
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        root_path = "./dataset/ETT-small/"
+        data_path = "ETTm2.csv"
+
+        model_meta_info = {
+            "root_path": root_path,
+            "data_path": data_path,}
+        store_meta_info(model_meta_info)
+
+if __name__ == '__main__':
+    unittest.main()
